38cb8597b13e1026b7749c1c36eda97dbcf7df47
杨英 <yangying@yangyingdeMacBook-Pro.local>
2017-11-15 19:38:06 +0800

    处理冲突

--- src/main/java/com/wind/auth/WindAuthServiceApplication.java
+++ src/main/java/com/wind/auth/WindAuthServiceApplication.java
@@ -4,7 +4,6 @@ import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 import com.wind.common.DataSourceEnvironment;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.*;
@@ -16,7 +15,6 @@ import org.springframework.context.annotation.Import;
         ServerPropertiesAutoConfiguration.class, WebMvcAutoConfiguration.class })
 @SpringBootApplication
 @MapperScan("com.wind.auth.mapper")
-@EnableAutoConfiguration
 @DubboComponentScan(basePackages = "com.wind.auth.service.impl")
 public class WindAuthServiceApplication {
 
